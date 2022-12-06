c8ce6dc3688688cc2ab2411e4c820a4875a4766e
Cameron Hummels <chummels@gmail.com>
2022-12-05 18:18:08 -0800

    Hiding tests by default.

--- skymap.py
+++ skymap.py
@@ -32,6 +32,8 @@ def DM_ray():
     pass
 
 if __name__ == '__main__':
+    test = False
+
     #pts = np.array([[1,0,0], [0,1,0], [0,0,1]])
     #print(appendSpherical_np(pts))
     #for pt in pts:
@@ -45,15 +47,12 @@ if __name__ == '__main__':
     phis = np.radians(phis)
     thetas = np.radians(thetas)
     xs, ys, zs = sph2cart(phis, thetas, rs)
-    zs = np.abs(zs)
 
-    #rands = np.random.randn(*rs.shape)
-    #zz = np.sqrt(phis**2 + thetas**2)
-
-    #h = plt.contourf(phis, thetas, zz)
-    h = plt.contourf(xs, ys, zs)
-    plt.axis('scaled')
-    plt.colorbar()
-    plt.savefig('test.png')
+    if test:
+        zs = np.abs(zs)
+        h = plt.contourf(xs, ys, zs)
+        plt.axis('scaled')
+        plt.colorbar()
+        plt.savefig('test.png')
 
     # make aitoff projection
